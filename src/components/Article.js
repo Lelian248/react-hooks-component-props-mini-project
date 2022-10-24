6c504ad704be8c2865b740fdb4c11ff267b4b3cf
lilian manaa <lelian.ibraheem@gmail.com>
2022-10-24 17:31:24 +0300

    Completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
-export default function Article({title,date="January 1, 1970",preview}) {
-  return (
+export default function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    function roundNearest5(num) {
+        return Math.round(num / 5);
+    }
+    function roundNearest10(num) {
+        return Math.round(num / 10);
+    }
+
+    const emoji =  ()=> {
+        const emojiArray =[]
+        if ({minutes} < 30){
+            for (let i = 0; i <= roundNearest5(minutes ); i++) {
+                emojiArray.push("☕️");
+            }
+            return (emojiArray.join(' ') + ' ' +  minutes  + " mins to read")
+        }else{
+            for (let i = 0; i <= roundNearest10( minutes ); i++) {
+                emojiArray.push("🍱");
+            }
+            return emojiArray.join('') + " " +  minutes  + " mins to read";
+
+        }
+    }
+
+return (
     <article>
         <h3>{title}</h3>
         <small>{date}</small>
         <p>{preview}</p>
+        <span>{emoji}</span>
     </article>
-  )
+)
 }
